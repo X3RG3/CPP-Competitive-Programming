e2567f6ddc61361bb421ef3080ba3d2b3d9d1f9e
X3RG3 <irfanayon88@gmail.com>
2025-11-29 01:29:51 +0600

    input-output

---
+++ 1.Basics/input-output.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+using namespace std;
+int main(){
+    int x;
+    int y;
+    cout<<"Enter Value for x : ";
+    cin>>x;
+    cout<<"Enter Value for y : ";
+    cin>>y;
+
+    cout<<"X is  : "<<x<<endl;
+    cout<<"Y is  : "<<y<<endl;
+}
