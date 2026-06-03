c03d7ba6562ab2436f7509ef3323c516475c72e8
X3RG3 <irfanayon88@gmail.com>
2026-06-03 18:06:53 +0600

    Module 1 switch

---
+++ Module 1/switch.cpp
@@ -0,0 +1,37 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    
+    int day;
+    cin>> day;
+
+    switch(day){
+
+        case 1 :
+            cout<<"Saturday\n";
+            break;
+        case 2 :
+            cout<<"Sunday\n";
+            break;
+        case 3 :
+            cout<<"Monday\n";
+            break;
+        case 4 :
+            cout<<"Monday\n";
+            break;
+        case 5 :
+            cout<<"Monday\n";
+            break;
+        case 6 :
+            cout<<"Monday\n";
+            break;
+        case 7 :
+            cout<<"Monday\n";
+            break;
+        default : 
+            cout<<"Wrong input\n";
+    }
+
+    return 0;
+}
