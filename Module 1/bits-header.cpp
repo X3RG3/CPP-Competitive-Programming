43c23ea312611b2eb8c93f9cfa9c05201f47c490
X3RG3 <irfanayon88@gmail.com>
2026-06-04 00:35:16 +0600

    Module 1 bits header

---
+++ Module 1/bits-header.cpp
@@ -0,0 +1,14 @@
+#include <bits/stdc++.h>
+using namespace std;
+int main(){
+    int a,b;
+    cin>>a>>b;
+    cout<< " a : "<<a<<endl;
+    cout<< " b : "<<b<<endl;
+    swap(a,b);
+
+    cout<< " a : "<<a<<endl;
+    cout<< " b : "<<b<<endl;
+
+    return 0;
+}
