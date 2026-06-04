ad00e5e18a9a627bd9769d020a1106a6027d5684
X3RG3 <irfanayon88@gmail.com>
2026-06-04 22:31:05 +0600

    Module 1 dynamic memory

---
+++ Module 2/dynamic-mem.cpp
@@ -0,0 +1,11 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+    int x = 10;
+    //  new data_type 
+    int *p = new int ;
+    *p = 10;
+    cout<< *p << endl;
+    return 0;
+}
