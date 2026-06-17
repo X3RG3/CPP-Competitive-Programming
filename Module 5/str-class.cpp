c34a5e7919c23374f61225c2d88a64b145c99e57
X3RG3 <irfanayon88@gmail.com>
2026-06-17 18:29:49 +0600

    string

---
+++ Module 5/str-class.cpp
@@ -0,0 +1,16 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+    string s = "Hello";
+    string s2 = "Hello";
+    if(s==s2){
+        cout<<"same"<<endl;
+    }
+    else{
+        cout<<"Not Same"<<endl;
+    }
+    string s3 = "Hello , this is a string class";
+    cout<<s3<<endl;
+    return 0;
+}
