944d1e7bd15c0a189e7d6eb49767f621b7129d3b
X3RG3 <irfanayon88@gmail.com>
2026-06-18 08:50:54 +0600

    mis

--- A_Medium_Number.cpp
+++ A_Medium_Number.cpp
@@ -7,38 +7,12 @@ int main() {
     for(int i=0;i<t;i++){
         int a,b,c;
         cin>>a>>b>>c;
-        int max,min;
-        max = a;
-        min = a;
-        if(a> max){
-            max = a;
-        }
-        else if(b> max){
-            max = b;
-        }
-        else if(c> max){
-            max = c;
-        }
+        int mx,mn;
+        mx = max({a,b,c});
+        mn = min({a,b,c});
 
-        if(a< min){
-            min = a;
-        }
-        else if(b< min){
-            min = b;
-        }
-        else if(c< min){
-            min = c;
-        }
-
-        if( a< max && a>min){
-            cout<<a<<endl;
-        }
-        else if(b<max && b >min){
-            cout<<b<<endl;
-        }
-        else if(c<max && c> min){
-            cout<<c<<endl;
-        }
+        cout<<a+b+c - mx-mn<<endl;
+        
 
     }
     return 0;
