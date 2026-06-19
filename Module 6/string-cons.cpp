97328f1bb14c6d6dfa3a09ec9f03bfc389c15b66
X3RG3 <irfanayon88@gmail.com>
2026-06-19 11:59:50 +0600

    string constr

---
+++ Module 6/string-cons.cpp
@@ -0,0 +1,17 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+    string s = "Hello";
+    string s2("World");
+    cout<<s<<endl;
+    cout << s2<<endl;
+    string s3("Hello world",4);
+    cout<<s3<<endl;
+    string t(s,1);
+    cout<<t<<endl;
+
+    string s4(5,'A');
+    cout<< s4<<endl;
+    return 0;
+}
