19daa44ce4860df1148309714210992b35e742ea
X3RG3 <irfanayon88@gmail.com>
2026-08-06 00:00:55 +0600

    doubly linked list

---
+++ Data Structure/Module 9/doubly_insert_at_head.cpp
@@ -0,0 +1,58 @@
+#include <bits/stdc++.h>
+using namespace std;
+class Node{
+    public:
+    int value;
+    Node* next;
+    Node* prev;
+    Node(int value){
+        this->value = value;
+        this->next = NULL;
+        this->prev = NULL;
+    }
+};
+
+void print_forward(Node* head){
+    Node* temp = head;
+    while(temp != NULL){
+        cout<< temp->value<<" ";
+        temp = temp->next;
+    }
+    cout<<endl;
+}
+
+void insert_at_head(Node* &head,Node* tail, int val){
+    Node* newNode = new Node(val);
+
+     if(head = NULL){
+        head = newNode;
+        tail = newNode;
+        return;
+    }
+
+    newNode->next = head;
+    head->prev = newNode;
+
+    head = newNode;
+}
+
+int main() {
+    Node* head = new Node(10);
+    Node* a = new Node(20);
+    Node* tail = new Node(30);
+
+    head->next = a;
+    a->prev = head;
+    a->next = tail;
+
+    tail->prev = a;
+
+    print_forward(head);
+
+    insert_at_head(head,tail, 100);
+
+    print_forward(head);
+
+
+    return 0;
+}
